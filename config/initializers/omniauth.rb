efe91f2a21f00d402f94204762ed17dc7eff401c
spectre6000 <spectre6000@gmail.com>
2015-06-08 14:00:59 -0600

    facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '496139213872435', 'c63092f202297ddc1ed97470bbeae10a'
 end
