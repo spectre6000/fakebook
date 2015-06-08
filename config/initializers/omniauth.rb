a1f6b026f10655025792c565d5ed25460a74babd
spectre6000 <spectre6000@gmail.com>
2015-06-08 14:24:40 -0600

    facebook login successful

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '496139213872435', 'c63092f202297ddc1ed97470bbeae10a'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "user/registrations" }
+  devise_for :users, controllers: { registrations: "user/registrations", :omniauth_callbacks => "user/omniauth_callbacks" }
   
   root to: "users#index"
 
   resources :users, except: [:create, :new]
+
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
   
 end
