804031f24e6c293a228191b679a1d00db15fad65
spectre6000 <spectre6000@gmail.com>
2015-06-09 10:02:12 -0600

    friend model

---
+++ db/migrate/20150609155833_create_friends.rb
@@ -0,0 +1,15 @@
+class CreateFriends < ActiveRecord::Migration
+  def change
+    create_table :friends do |t|
+      t.integer :friend_id
+      t.integer :friended_id
+      t.boolean :accepted, default: false
+
+      t.timestamps null: false
+    end
+
+    add_index :friends, :friend_id
+    add_index :friends, :friended_id
+    add_index :friends, [:friend_id, :friended_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608192158) do
+ActiveRecord::Schema.define(version: 20150609155833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "friend_id"
+    t.integer  "friended_id"
+    t.boolean  "accepted",    default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "friends", ["friend_id", "friended_id"], name: "index_friends_on_friend_id_and_friended_id", unique: true, using: :btree
+  add_index "friends", ["friend_id"], name: "index_friends_on_friend_id", using: :btree
+  add_index "friends", ["friended_id"], name: "index_friends_on_friended_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
